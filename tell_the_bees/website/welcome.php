007328116ffbf2ae8d742d08c54df7c4fdbfdb51
MahonriRay <MahonriRay@byui.edu>
2022-12-02 00:29:09 -0700

    welcome php

--- tell_the_bees/website/welcome.php
+++ tell_the_bees/website/welcome.php
@@ -6,3 +6,63 @@ Your email address is: <?php echo $_POST["email"]; ?>
 
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="/WDD130/tell_the_bees/styles/style.css">
+    <title>Tell The Bees!</title>
+</head>
+<body>
+    <div id="content">
+        <header>
+            <a id="logo_link" href="index.html">
+                <img class="logo" src="/WDD130/tell_the_bees/images/logo1.png" alt="Tell The Bees Logo">
+            </a>
+            <nav>
+                <a href="index.html">Home</a>
+                <a href="/WDD130/tell_the_bees/website/resources.html">Resources</a>
+                <a href="/WDD130/tell_the_bees/website/guides.html">Guides</a>
+                <a href="contactus.html" target="_blank">Contact Us</a>
+            </nav>
+        </header>
+        <div id="home-title">
+            <h1> Tell The Bees!</h1>
+        </div>
+        <main class ="home-grid">
+            <img class="top-left-dl" src="/WDD130/tell_the_bees/images/dotted-line.png" alt="dotted line">
+            <img class="bottom-right-dl" src="/WDD130/tell_the_bees/images/dotted-line2.png" alt="dotted line">
+            <img class="honeycomb-img" src="/WDD130/tell_the_bees/images/honeycomb.jpg" alt="Honeycomb Photo">
+            <img class="flower-bee" src="/WDD130/tell_the_bees/images/flower_bee.jpg" alt="Bee on a flower">
+            <p>For as many beekeepers that are in the world, there are just as many reasons for why these individuals care for bees. Asides from the sweet, sweet honey that you can obtain from such a rewarding task as beekeeping, it is a super humbling experience that teaches patience and how to care for delicate living animals. Whether you’re reasoning to keep bees is for the honey, to add another sort of “pet” to your family, or it’s purely for hobby, I hope that you’ll be able to find guidance in my website as you embark on your journey of beekeeping!<br>
+            <br><br></p>
+            <img class="bee-png" src="/WDD130/tell_the_bees/images/bee-18976.png" alt="little bee icon">
+            <p>The Bee is domesticated, but not tamed.</p>
+            <p>~William Longgood</p>
+        </main>
+            <div id="sign-up">
+                Welcome <?php echo $_POST["name"]; ?><br>
+                Your email address is: <?php echo $_POST["email"]; ?>
+            </div>
+        </section>
+        <footer>
+            <p>Tell The Bees &copy; 2022 - Mahonri Ray</p>
+            <p><a href="/WDD130/tell_the_bees/site-plan.html" target="_blank">Site Plan</a></p>
+            <p><a href="contactus.html" target="_blank">Contact Us</a></p>
+            <div class="social">
+                <a href="https://facebook.com" target="_blank">
+                    <img src="/WDD130/tell_the_bees/images/facebook.png" alt="facebook icon">
+                </a>
+                <a href="https://twitter.com" target="=_blank">
+                    <img src="/WDD130/tell_the_bees/images/twitter.png" alt="twitter icon">
+                </a>
+                <a href="https://instagram.com" target="=_blank">
+                    <img src="/WDD130/tell_the_bees/images/instagram.png" alt="instagram icon">
+                </a>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
